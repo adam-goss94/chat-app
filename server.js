71dc16b1b94ab8452bb76bc0c53a84b13639db52
Adam Goss <adam.goss94@gmail.com>
2020-06-21 18:31:49 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const messages = [];
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  })
